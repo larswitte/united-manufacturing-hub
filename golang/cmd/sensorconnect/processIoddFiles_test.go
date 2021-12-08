40cb932a51ae837af02c0af392bc49fa8b6220c7
larswitte <lars.witte@rwth-aachen.de>
2021-12-08 09:58:30 +0100

    fix: solved problem with bracket

--- golang/cmd/sensorconnect/processIoddFiles_test.go
+++ golang/cmd/sensorconnect/processIoddFiles_test.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"reflect" //for reading out type of variable
+	"testing"
 )
 
-
-func TestAddLowSpeedStates_1(t *testing.T) {
+func TestUnmarshalIoddFile(t *testing.T) {
 	//Read File
 	dat, err := ioutil.ReadFile("C:/Users/LarsWitte/umh_larswitte_repo/sensorconnectRep/united-manufacturing-hub/golang/cmd/sensorconnect/ifm-0002BA-20170227-IODD1.1.xml")
 	if err != nil {
@@ -24,15 +24,13 @@ func TestAddLowSpeedStates_1(t *testing.T) {
 	}
 
 	//should give out 698 and int
-	if !reflect.DeepEqual(ioDevice.ProfileBody.DeviceIdentity.DeviceId), 698) {
+	if !reflect.DeepEqual(ioDevice.ProfileBody.DeviceIdentity.DeviceId, 698) {
 		t.Error()
 	}
 
 	if !reflect.DeepEqual(reflect.TypeOf(ioDevice.ProfileBody.DeviceIdentity.DeviceId), "int") {
 		t.Error()
 	}
-	
-
 
 	//should put out 4 and int
 	fmt.Println(ioDevice.ProfileBody.DeviceFunction.ProcessDataCollection.ProcessData.ProcessDataIn.Datatype.ReccordItem[1].SimpleDatatype.BitLength)
